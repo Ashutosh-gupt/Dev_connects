e7e1ec22726fdc1eea9a17b85fc477b1b587fcb1
AshutoshGupta <rockashu.30.ar@gmail.com>
2022-04-08 12:02:51 +0530

    initial commit
    Signed-off-by: AshutoshGupta <rockashu.30.ar@gmail.com>

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`server Started on port ${PORT}`));
